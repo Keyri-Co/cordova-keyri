13efabe4ed184121c1d170f324a956e23863331e
Aditya Malladi <aditya@keyri.co>
2023-05-09 16:17:59 -0400

    update plugin

--- src/iOS/com/keyri/cordova/plugin/CordovaKeyri.swift
+++ src/iOS/com/keyri/cordova/plugin/CordovaKeyri.swift
@@ -63,6 +63,20 @@ import keyri_pod
         commandDelegate.send(result, callbackId: command.callbackId)
     }
     
+    @objc(listUniqueAccounts:)
+    func listUniqueAccounts(command: CDVInvokedUrlCommand) {
+        guard let keyri = keyri else {
+            let error = "Keyri object not initialized. Please call InitializeKeyri first"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error)
+            commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            return
+        }
+        
+        let list = keyri.listUniqueAccounts()
+        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: list ?? [:])
+        commandDelegate.send(result, callbackId: command.callbackId)
+    }
+    
     @objc(getAssociationKey:)
     func getAssociationKey(command: CDVInvokedUrlCommand) {
         guard let keyri = keyri else {
@@ -127,5 +141,58 @@ import keyri_pod
         return
     }
     
+    @objc(removeAssociationKey:)
+    func removeAssociationKey(command: CDVInvokedUrlCommand) {
+        guard let keyri = keyri else {
+            let error = "Keyri object not initialized. Please call InitializeKeyri first"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error)
+            commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            return
+        }
+        
+        guard let username = command.arguments[0] as? String else {
+            let error = "Invalid arguments"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error)
+            commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            return
+        }
+        
+        do {
+            try keyri.removeAssociationKey(publicUserId: username)
+            let result = CDVPluginResult(status: CDVCommandStatus_OK)
+            commandDelegate.send(result, callbackId: command.callbackId)
+        } catch {
+            let result = CDVPluginResult(status: CDVCommandStatus_ERROR)
+            commandDelegate.send(result, callbackId: command.callbackId)
+        }
+    }
+    
+    @objc(getUserSignature:)
+    func getUserSignature(command: CDVInvokedUrlCommand) {
+        guard let keyri = keyri else {
+            let error = "Keyri object not initialized. Please call InitializeKeyri first"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error)
+            commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            return
+        }
+        
+        guard let username = command.arguments[0] as? String,
+              let dataStr = command.arguments[1] as? String,
+              let customData = dataStr.data(using: .utf8) else {
+            let error = "Invalid arguments"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error)
+            commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            return
+        }
+        
+        do {
+            let signature = try keyri.generateUserSignature(for: username, data: customData)
+            let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: signature.rawRepresentation.base64EncodedString())
+            commandDelegate.send(result, callbackId: command.callbackId)
+        } catch {
+            let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "User not found")
+            commandDelegate.send(result, callbackId: command.callbackId)
+        }
+    }
 }
 
